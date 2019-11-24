36e1b8960ff12a34b167f56b05cac118b16247bc
Cicy29 <47091978+Cicy29@users.noreply.github.com>
2019-11-24 14:30:22 +0100

    Update user.js
    add user schema

--- week-5/models/user.js
+++ week-5/models/user.js
@@ -27,7 +27,7 @@ const UserSchema = new mongoose.Schema({
     }]
 })
 
-PersonSchema.methods.findPeersUnder30 = function (cb) {
+UserSchema.methods.findPeersUnder30 = function (cb) {
     return UserModel.find({
         age: {
             $gte: 30
@@ -44,35 +44,3 @@ module.exports = UserModel
 
 
 
-module.exports = class User {
-    constructor(name, age, location, suggestedEvent = [], club = [], id) {
-        this.name = name;
-        this.age = age;
-        this.location = location;
-        this.suggestedEvent = suggestedEvent;
-        this.club = club;
-        this.id = id;
-    }
-    wantsToPartyAt(club) {
-        this.club = club.name;
-        console.log(
-            "Welcome " + this.name + " ," + "you will have the time of your life!"
-        );
-        club.clubbers.push(this);
-    }
-    buysTicket(suggestedEvent) {
-        this.suggestedEvent = suggestedEvent.genre;
-        console.log(this.name + ", have fun there, life is too short!!");
-        suggestedEvent.attendees.push(this);
-    }
-    static create({
-        name,
-        age,
-        location,
-        suggestedEvent,
-        club,
-        id
-    }) {
-        return new User(name, age, location, suggestedEvent, club, id);
-    }
-};
